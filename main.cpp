5367a06c0068993d13750d0dcba61e4f665c75ce
Samuel Chen <dismany0@gmail.com>
2024-05-10 08:58:37 -0400

    moved files around

--- main.cpp
+++ main.cpp
@@ -81,7 +81,7 @@ int main() {
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
-    cam.samples_per_pixel = 10;
+    cam.samples_per_pixel = 25;
     cam.max_depth = 25;
 
     cam.vfov     = 70;
