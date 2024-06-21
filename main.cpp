22267361c395b35c7d67fe5e2528ce648014cb17
Samuel Chen <dismany0@gmail.com>
2024-06-21 00:56:58 -0400

    image 2

--- main.cpp
+++ main.cpp
@@ -151,9 +151,31 @@ void world_2_checkered_spheres(){
     cam.render(world);
 }
 
+void earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(0,0,12);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
 int main() {
     // world_1();
     // world_2();
-    world_2_checkered_spheres();
+    // world_2_checkered_spheres();
+    earth();
     return 0;
 }
