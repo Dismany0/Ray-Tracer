aecf3a60f0ed1a8eaad76b8d8154dc595161b591
Samuel Chen <samue.chen@mail.utoronto.ca>
2024-07-02 15:08:43 -0400

    scale

--- main.cpp
+++ main.cpp
@@ -184,7 +184,7 @@ void perlin_spheres()
 {
     hittable_list world;
 
-    auto pertext = make_shared<noise_texture>();
+    auto pertext = make_shared<noise_texture>(1);
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
     world.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
