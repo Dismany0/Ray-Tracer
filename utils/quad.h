9da6d23f3b6959449eec6886be37ef0262069976
Samuel Chen <samue.chen@mail.utoronto.ca>
2024-07-04 14:51:52 -0400

    Update quad.h

--- utils/quad.h
+++ utils/quad.h
@@ -9,6 +9,12 @@ class quad : public hittable {
     quad(const point3& Q, const vec3& u, const vec3& v, sared_ptr<material> mat) :
     Q(Q), u(u), v(v), mat(mat)
     {
+
+        //Plnar coords
+        auto n = cross(u, v);
+        normal = unit_vector(n);
+        D = dot(normal, Q);
+
         set_bounding_box();
     }
 
@@ -21,6 +27,27 @@ class quad : public hittable {
     aabb bounding_box() const override { return bbox; }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        auto denom = dot(normal, r.direction());
+
+        //parallel to plane = nohit
+        if (fabs(denom) < 1e-8)
+            return false;
+
+            //return false if t is outside interval for ray
+            auto t = (D - dot(normal, r.origin())) / denom;
+            if (!ray_t.contains(t))
+                return false;
+
+            //otherwise hits the plane
+            auto intersection = r.at(t);
+
+            rec.t = t;
+            rec.p = intersection;
+            rec.mat = mat;
+            rec.set_face_normal(r, normal);
+            
+            return true;
+
         return false; 
     }
 
@@ -29,5 +56,7 @@ class quad : public hittable {
     vec3 u, v;
     shared_ptr<material> mat;
     aabb bbox;
+    vec3 normal;
+    double D;
 
 }
