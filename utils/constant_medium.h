785eb42073848e82085133429b10a9864070e8fb
Samuel Chen <dismany0@gmail.com>
2024-07-15 00:58:26 -0400

    Create constant_medium.h

---
+++ utils/constant_medium.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANT_MEDIUM_H
+#define CONSTANT_MEDIUM_H
+
+#include "rtweekend.h"
+
+#include "hittable.h"
+#include "material.h"
+#include "texture.h"
+
+class constant_medium : public hittable {
+    public:
+
+    private:
+
+};
+
+#endif
